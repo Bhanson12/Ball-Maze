d04883af9b3ade770c26f17bb9b17b6fdf91dd1b
Braydon Hanson <hanson9b@udml14.local>
2018-11-14 16:39:51 -0600

    initialize sceneKit

--- Ball Maze/GameViewController.swift
+++ Ball Maze/GameViewController.swift
@@ -11,8 +11,14 @@ import SceneKit
 
 class GameViewController: UIViewController {
     
+    var scnView: SCNView!
+    var scnScene: SCNScene!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupView()
+        setupScene()
     }
     
     override var shouldAutorotate: Bool {
@@ -21,6 +27,15 @@ class GameViewController: UIViewController {
     
     override var prefersStatusBarHidden: Bool {
         return true
+    }
+    
+    func setupView() {
+        scnView = self.view as! SCNView
+    }
+    
+    func setupScene() {
+        scnScene = SCNScene()
+        scnView.scene = scnScene
     }
 }
 
