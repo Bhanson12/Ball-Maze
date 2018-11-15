6aea9d98c9ce64b65801a7892d4b84c0c58c6d4e
Braydon Hanson <hanson9b@udml14.local>
2018-11-14 18:36:24 -0600

    added main scene with ball

--- Ball Maze/GameViewController.swift
+++ Ball Maze/GameViewController.swift
@@ -13,12 +13,14 @@ class GameViewController: UIViewController {
     
     var scnView: SCNView!
     var scnScene: SCNScene!
+    var cameraNode: SCNNode!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupView()
         setupScene()
+        setupCamera()
     }
     
     override var shouldAutorotate: Bool {
@@ -31,11 +33,19 @@ class GameViewController: UIViewController {
     
     func setupView() {
         scnView = self.view as! SCNView
+        scnView.allowsCameraControl = true
     }
     
     func setupScene() {
-        scnScene = SCNScene()
+        scnScene = SCNScene(named: "BallMaze.scnassets/MainScene.scn")
         scnView.scene = scnScene
+    }
+    
+    func setupCamera() {
+        cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        cameraNode.position = SCNVector3(x: 0, y: 0, z: 10) //Position camera
+        scnScene.rootNode.addChildNode(cameraNode)
     }
 }
 
