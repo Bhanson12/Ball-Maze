d1edd0a4d3365da02e175502e2ed9016712fa4d7
Braydon Hanson <hanson9b@udml14.local>
2018-11-15 22:56:10 -0600

    added core motion code

--- Ball Maze/GameViewController.swift
+++ Ball Maze/GameViewController.swift
@@ -8,12 +8,16 @@
 
 import UIKit
 import SceneKit
+import CoreMotion
 
 class GameViewController: UIViewController {
     
     var scnView: SCNView!
     var scnScene: SCNScene!
     var cameraNode: SCNNode!
+    var ball: SCNNode!
+    var motionManager: CMMotionManager!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +25,8 @@ class GameViewController: UIViewController {
         setupView()
         setupScene()
         setupCamera()
+        setupBall()
+        setupMotion()
     }
     
     override var shouldAutorotate: Bool {
@@ -47,5 +53,38 @@ class GameViewController: UIViewController {
         cameraNode.position = SCNVector3(x: 0, y: 0, z: 10) //Position camera
         scnScene.rootNode.addChildNode(cameraNode)
     }
+    
+    func setupBall() {
+        ball = scnScene.rootNode.childNode(withName: "ball reference", recursively: true)
+        //ball!.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
+    }
+    
+    // NEED TO TEST WITH PHONE
+    func setupMotion(){
+        motionManager = CMMotionManager()
+        
+        if motionManager.isGyroAvailable {
+            motionManager.gyroUpdateInterval = 1.0 / 60.0
+            motionManager.startGyroUpdates()
+            
+            let data = motionManager.gyroData
+            let x = data?.rotationRate.x
+            let y = data?.rotationRate.y
+            let z = data?.rotationRate.z
+            print(x!, y!, z!)
+            
+            // Need to update ball position after checking values
+        }
+            
+            // Just testing moving the ball
+        else{
+            if(ball.position.x >= 0){
+                ball.runAction(SCNAction.moveBy(x: -0.5, y: 0, z: 0, duration: 5))
+            } else {
+                ball.runAction(SCNAction.moveBy(x: 0.5, y: 0, z: 0, duration: 5))
+            }
+        }
+    }
+
 }
 
